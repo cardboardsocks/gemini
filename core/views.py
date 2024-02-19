a619116cb12a0bb97124c333690703242d0e251b
Ben Mckenzie <bmckenzie0522@gmail.com>
2024-02-18 20:02:27 -0700

    added view for image upload

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageUploadForm
+from .models import Image
 
-# Create your views here.
+def image_upload(request):
+   if request.method == 'POST':
+       form = ImageUploadForm(request.POST, request.FILES)
+       if form.is_valid():
+           new_image = form.save(commit=False)
+           new_image.uploader = request.user 
+           new_image.save()
+           return redirect('image_list')  # Redirect to the image list view
+   else:
+       form = ImageUploadForm()
+   return render(request, 'upload.html', {'form': form})
