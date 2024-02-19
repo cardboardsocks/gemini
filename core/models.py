9878c2e6a2b81cebfc3e4731c90a4124ccba0698
Ben Mckenzie <bmckenzie0522@gmail.com>
2024-02-18 19:48:55 -0700

    added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    caption = models.TextField(blank=True)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    num_up_vote = models.IntegerField(default=0)
+    num_down_vote = models.IntegerField(default=0)
