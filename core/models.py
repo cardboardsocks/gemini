517d75e5019165e22af5b5c2e1398f2902b44b33
Ben Mckenzie <bmckenzie0522@gmail.com>
2024-02-26 21:39:33 -0700

    added Comment model

--- core/models.py
+++ core/models.py
@@ -10,3 +10,9 @@ class Image(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     num_up_vote = models.IntegerField(default=0)
     num_down_vote = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
