74ca9fd423f7a69e45954fc706fa3d2149c7f714
Ben Mckenzie <bmckenzie0522@gmail.com>
2024-03-02 14:05:14 -0700

    added the only one vote per user functionality

--- core/views.py
+++ core/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
-from .models import Image
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.http import JsonResponse
+from .models import Image
 
 def like_image(request, image_id):
     if request.method == 'POST':
         image = get_object_or_404(Image, pk=image_id)
-        image.num_up_vote += 1
-        image.save()
-        return JsonResponse({'updated_like_count': image.num_up_vote}) 
+        if request.user not in image.voters.all(): 
+            image.num_up_vote += 1
+            image.voters.add(request.user)
+            image.save()
+            return JsonResponse({'updated_like_count': image.num_up_vote})
+        else:
+            return JsonResponse({'error': 'You have already liked this image'})
     else:
-        # Handle non-AJAX requests if needed
-        pass 
+        return redirect('image_list')  # Redirect to the image list on non-POST requests
+
 
 def dislike_image(request, image_id):
     if request.method == 'POST':
         image = get_object_or_404(Image, pk=image_id)
-        image.num_down_vote += 1
-        image.save()
-        return JsonResponse({'updated_dislike_count': image.num_down_vote}) 
+        if request.user not in image.voters.all(): 
+            image.num_down_vote += 1
+            image.voters.add(request.user)
+            image.save()
+            return JsonResponse({'updated_dislike_count': image.num_down_vote})
+        else:
+            return JsonResponse({'error': 'You have already disliked this image'})
     else:
-        # Handle non-AJAX requests if needed
-        pass
+        return redirect('image_list') 
+
 
 def image_upload(request):
    if request.method == 'POST':
